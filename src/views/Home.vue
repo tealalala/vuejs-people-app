8e3531d65c119bc7bf386dfe68fc3ca8cace9600
Tea <tdinhla@protonmail.com>
2018-11-18 10:09:45 -0800

    add form to add new person, counter, button to delete person, and toggle hide/show of person

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,24 @@
   <div class="home">
     <h1>{{ message }}</h1>
 
+    <!-- People List -->
+    <p>counter: {{ people.length }}</p>
+
     <ol>
-      <li v-for="person in people">
-        name: {{ person.name }},
-        bio: {{ person.bio }},
-        bioVisible: {{ person.bioVisible }}
+      <li v-for="person in people" v-on:click="toggleHide()">
+        full_name: {{ person.full_name }},
+        bio: {{ person.bio }}
+        <button v-on:click="removePerson(person.id)">Remove this Person</button>
       </li>
     </ol>
 
+
+    <!-- Form to Add Person to People List -->
+    <!-- <form class="" action="index.html" method="post"> -->
+      <p>full_name: <input type="text" v-model="newPerson.full_name"></p>
+      <p>bio: <input type="text" v-model="newPerson.bio"></p>
+      <button v-on:click="addPerson()">Add Person</button>
+    <!-- </form> -->
 
   </div>
 </template>
@@ -23,14 +33,29 @@ export default {
     return {
       message: "People App",
       people: [
-        {name: "Abby", bio: "likes apples", bioVisible: true},
-        {name: "Christine", bio: "likes cherries", bioVisible: true},
-        {name: "Dalena", bio: "likes danishes", bioVisible: true}
-      ]
+        {full_name: "Abby", bio: "likes apples", bioVisible: true},
+        {full_name: "Betsy", bio: "likes buttons", bioVisible: true},
+        {full_name: "Christine", bio: "likes cherries", bioVisible: true}
+      ],
+      newPerson: {full_name: "", bio: "", bioVisible: true}
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    addPerson: function() {
+      console.log("add person button");
+      this.people.push(this.newPerson);
+      this.newPerson = {full_name: "", bio: "", bioVisible: true};
+    },
+    removePerson: function(inputPerson) {
+      var index = this.people.indexOf(inputPerson);
+      this.people.pop(index);
+    },
+    toggleHide: function () {
+      var liTag = document.querySelector('li');
+      liTag.classList.toggle('hidden');
+    }
+  },
   computed: {}
 };
 </script>
